419f8e9e9cd67676f37e36e8b45f25972b81b647
Daniel Duarte <danielduartego@gmail.com>
2015-12-09 17:49:18 -0800

    improve dashboard style

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,14 +1,14 @@
 class DashboardsController < ApplicationController
 
   def show
-    @user = User.find params[:id]
-    @users = User.all 
-    @projects = Project.all
-    @project = Project.new
+
   end
 
   def index
-
+    @users = User.all
+    @projects = Project.all
+    @project = Project.new
+    @calendars = Calendar.all
   end
 
 
